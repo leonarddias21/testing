4ea40bd254cfb0f32256218411e30ca15642f607
Leonard Dias <leonarddias21@gmail.com>
2023-04-15 20:21:16 +0530

    Testing

---
+++ Space.java
@@ -0,0 +1,13 @@
+package practise;
+
+public class Space {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1="TEST1234";
+		str1=str1.replaceAll("[^A-Z^]", "");
+		System.out.println(str1);
+
+	}
+
+}
